5bf0e8e062ae6a90b16a0ee5fe21ec3d72802ccc
red1k <erdenebayar_sh@yahoo.com>
2018-09-02 12:06:08 +0800

    Stack Imlementation - need to rewrite

--- StackArray.cpp
+++ StackArray.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <limits.h>
 
-#define MAX 1000;
+#define MAX 1000
 
 using namespace std;
 
@@ -10,13 +10,13 @@ class Stack {
 	int top;
 	public:
 		int a[MAX];					//Maximum size of Stack
-
 		Stack() {
 			top = -1;
 		}
 		int pop();
 		bool push(int x);
 		bool isEmpty();
+		void show();
 };
 
 bool Stack::push(int x) {
@@ -45,13 +45,27 @@ bool Stack::isEmpty() {
 	return (top < 0);
 }
 
+/*										need to rewrite show function
+void Stack::show() {
+	if (isEmpty()) {
+		cout << "Stack underflow \n";
+	}
+	for (int i = 0; i < MAX; i++)
+		cout << a[i];
+}
+*/
+
 
 int main() {
-
 	Stack s;
+
 	s.push(10);
 	s.push(20);
 	s.push(30);
+
+//	s.show();
+	cout << s.pop() << "\nPoped from stack\n";
+//	s.show();
 
 	return 0;
 
