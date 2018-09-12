6fd53ae87aea0b3dfc7ad0f03724c81d73ca2119
red1k <erdenebayar_sh@yahoo.com>
2018-09-12 20:59:29 +0800

    Added tasks

---
+++ 3/queue.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Queue {
+	int len;
+	int *dat;
+	int size;
+};
+int error = 0;
+const char error_msg[][50] = {
+	"",
+	"Queue duuren!",
+	"Queue xooson!"
+};
+
+void init(struct Queue *p, int n)
+{
+	p->len = n;
+	p->size = 0;
+	p->dat = (int *) malloc(sizeof(int) * n);
+}
+
+void release(struct Queue *p)
+{
+	free(p->dat);
+	p->dat = NULL;
+}
+/* p-ийн зааж буй Queue хоосон бол 1 үгүй бол 0-ийг буцаана */
+int empty(struct Queue *p)
+{
+	/* Энд хоосон эсэхийг шалгах үйлдлийг хийнэ */
+}
+
+/* p-ийн зааж буй Queue-д x утгыг хийнэ */
+void push_back(struct Queue *p, int x)
+{
+	/* Энд оруулах үйлдлийг хийнэ үү */
+}
+
+/* p-ийн зааж буй Queue-с гарган буцаана */
+int pop_front(struct Queue *p)
+{
+	/* Энд гаргах үйлдлийг хийнэ үү */
+}
+
+/* p-ийн зааж буй Queue-н утгуудыг хэвлэнэ */
+void print(struct Queue *p)
+{
+	/* Энд хэвлэх үйлдлийг хийнэ үү */
+}
+
+/* p-ийн зааж буй Queue-н хамгийн эхний элементийн утгыг буцаана.
+   Гаргах үйлдэл хийхгүй.
+ */
+int front(struct Queue *p)
+{
+	
+}
+/* p-ийн зааж буй Queue-н хамгийн сүүлийн элементийн утгыг буцаана.
+   Queue-д өөрчлөлт оруулахгүй.
+ */
+int back(struct Queue *p)
+{
+	
+}
+/* p-ийн зааж буй Queue-д хэдэн элемент байгааг буцаана.
+   Queue-д өөрчлөлт оруулахгүй.
+ */
+int size(struct Queue *p)
+{
+	
+}
+
+
+int main()
+{
+	/* Queue үүсгэн, эхлүүлэх */
+	struct Queue st;
+	init(&st, 10);
+	int t, x;
+
+	while (1) {
+		printf("1: push_back, 2: pop_front, 3: print, 4: empty, 5: front,"
+		       "6: back, 7: size, 8: exit\n");
+		scanf("%d", &t);
+		error = 0;
+
+		switch (t) {
+		case 1:
+			scanf("%d", &x);
+			push_back(&st, x);
+			if (error) 
+				printf("Aldaa: %s\n", error_msg[error]);
+			else
+				printf("%d utga orloo\n", x);
+			break;
+		case 2:
+			x = pop_front(&st);
+			if (error) 
+				printf("Aldaa: %s\n", error_msg[error]);
+			else
+				printf("%d utga garlaa\n", x);
+			break;
+		case 3:
+			print(&st);
+			break;
+		case 4:
+			if (empty(&st))
+				printf("Queue xooson\n");
+			else
+				printf("Queue xooson bish\n");
+			break;
+		case 5:
+			x = front(&st);
+			if (error) 
+				printf("Aldaa: %s\n", error_msg[error]);
+			else
+				printf("%d utga xamgiin exend bna\n", x);
+			break;
+		case 6:
+			x = back(&st);
+			if (error) 
+				printf("Aldaa: %s\n", error_msg[error]);
+			else
+				printf("%d utga xamgiin suuld bna\n", x);
+			break;
+		case 7:
+			x = size(&st);
+			printf("Queue-d %d shirxeg element bna\n", x);
+			break;
+		default:
+			exit(0);
+		}
+	}
+	/* Queue-ийн эзэмшсэн нөөцийг чөлөөлөх */
+	release(&st);
+	return 0;
+}
---
+++ 3/stack.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack {
+	int len;
+	int *dat;
+	int size;
+};
+int error = 0;
+const char error_msg[][50] = {
+	"",
+	"Stack duuren!",
+	"Stack xooson!"
+};
+
+void init(struct Stack *p, int n)
+{
+	p->len = n;
+	p->size = 0;
+	p->dat = (int *) malloc(sizeof(int) * n);
+}
+
+void release(struct Stack *p)
+{
+	free(p->dat);
+	p->dat = NULL;
+}
+/* p-ийн зааж буй Stack хоосон бол 1 үгүй бол 0-ийг буцаана */
+int empty(struct Stack *p)
+{
+	/* Энд хоосон эсэхийг шалгах үйлдлийг хийнэ */
+}
+
+/* p-ийн зааж буй Stack-д x утгыг хийнэ */
+void push(struct Stack *p, int x)
+{
+	/* Энд оруулах үйлдлийг хийнэ үү */
+}
+
+/* p-ийн зааж буй Stack-с гарган буцаана */
+int pop(struct Stack *p)
+{
+	/* Энд гаргах үйлдлийг хийнэ үү */
+}
+
+/* p-ийн зааж буй Stack-н утгуудыг хэвлэнэ */
+void print(struct Stack *p)
+{
+	/* Энд хэвлэх үйлдлийг хийнэ үү */
+}
+
+
+int main()
+{
+	/* Stack үүсгэн, эхлүүлэх */
+	struct Stack st;
+	init(&st, 10);
+	int t, x;
+
+	while (1) {
+		printf("1: push, 2: pop, 3: print, 4: empty, 5: exit\n");
+		scanf("%d", &t);
+		error = 0;
+
+		switch (t) {
+		case 1:
+			scanf("%d", &x);
+			push(&st, x);
+			if (error) 
+				printf("Aldaa: %s\n", error_msg[error]);
+			else
+				printf("%d utga orloo\n", x);
+			break;
+		case 2:
+			x = pop(&st);
+			if (error) 
+				printf("Aldaa: %s\n", error_msg[error]);
+			else
+				printf("%d utga garlaa\n", x);
+			break;
+		case 3:
+			print(&st);
+			break;
+		case 4:
+			if (empty(&st))
+				printf("Stack xooson\n");
+			else
+				printf("Stack xooson bish\n");
+			break;
+		default:
+			exit(0);
+		}
+	}
+	/* Stack-ийн эзэмшсэн нөөцийг чөлөөлөх */
+	release(&st);
+	return 0;
+}
